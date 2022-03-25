3987479da323424d12351389420e5d58b6e90abd
MoizLokh <mlokhandwala360@gmail.com>
2022-03-24 20:30:29 -0700

    SPI error fixed - Tested output with oscilloscope

--- SPI_Experiment/main.c
+++ SPI_Experiment/main.c
@@ -5,6 +5,8 @@
  * main.c
  */
 
+int count = 0;
+
 void init_SPI(void);
 
 
@@ -13,9 +15,10 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	init_SPI();
+	PJDIR |= BIT0;
+	PJOUT &= ~BIT0;
 
 	__enable_interrupt();
-
 	for(;;);
 }
 
@@ -33,44 +36,51 @@ void init_SPI(void) {
     UCA0CTLW0 |= UCSYNC;        // Synchronous mode (SPI)
     UCA0CTLW0 |= UCMST;         // Master mode
 
-    //-- Setup Baud rate
 
-    UCA0BRW |= 0x6;
-    UCA0MCTLW |= UCOS16 + 0x2080; // config for baud 9600 with 1Mhz clk. OCOS16 - 1, UCBRF = 0x80 (Lower Byte) and UCBRS = 0x20 (Upper Byte)
+//    //-- Setup Baud rate
+//
+//    UCA0BRW |= 0x6;
+//    UCA0MCTLW |= UCOS16 + 0x2080; // config for baud 9600 with 1Mhz clk. OCOS16 - 1, UCBRF = 0x80 (Lower Byte) and UCBRS = 0x20 (Upper Byte)
 
     //-- Port configuration
 
     /* P2.1 for UCA0SOMI */
     P2SEL1 |= BIT1;
-    P2SEL0 &= BIT1;
+    P2SEL0 &= ~BIT1;
 
     /* P2.0 for UCA0SIMO */
     P2SEL1 |= BIT0;
-    P2SEL0 &= BIT0;
+    P2SEL0 &= ~BIT0;
 
     /* P1.5 for UCA0CLK */
     P1SEL1 |= BIT5;
-    P1SEL0 &= BIT5;
+    P1SEL0 &= ~BIT5;
 
 
-    UCA0CTLW0 &= UCSWRST;       // Leave reset mode before enabling interrupts
+    UCA0CTLW0 &= ~UCSWRST;       // Move out of reset mode before enabling interrupts
 
     //-- Enable Interrupts
 
     UCA0IE |= UCTXIE;           // Enable TX interrupts
-    UCA0IFG &= UCTXIFG;         // Clear interrupt flag
+    UCA0IFG |= UCTXIFG;         // Clear interrupt flag
 
 }
 
 #pragma vector = USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(void) {
+    PJOUT ^= BIT0;
     switch(__even_in_range(UCA0IV,18)) {
     case 0x00:  // No interrupt
         break;
     case 0x02:  // UCRXIFG
         break;
-    case 0x03:  // UCTXIFG
-        UCA0TXBUF = 0x04;
+    case 0x04:  // UCTXIFG
+        count++;
+        if (count < 10000) UCA0TXBUF= 0x09;
+        else {
+            count = 0;
+            UCA0IFG &= ~UCTXIFG;
+        }
         break;
     default:
         break;
